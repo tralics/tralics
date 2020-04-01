76665aa1c2e05d3cd124dae6c7ebb78e8795583c
Vincent Beffara <vbeffara@gmail.com>
2020-04-02 01:22:03 +0200

    clang-tidy does not like assert

--- src/tralics/types.h
+++ src/tralics/types.h
@@ -14,6 +14,6 @@ template <typename T> auto to_signed(T x) -> std::enable_if_t<std::is_unsigned_v
 }
 
 template <typename T> auto to_unsigned(T x) -> std::enable_if_t<std::is_signed_v<T>, std::make_unsigned_t<T>> {
-    assert(x >= 0);
+    assert(x >= 0); // NOLINT
     return static_cast<std::make_unsigned_t<T>>(x);
 }
