793e07521cd2a9fd2f1a4b855728d61112820d07
Vincent Beffara <vbeffara@gmail.com>
2020-05-07 23:33:11 +0200

    Remove warnings from robin_hood

--- src/Istring.cpp
+++ src/Istring.cpp
@@ -7,7 +7,7 @@ namespace {
     std::vector<std::string> SH{"", "", " "};
 
     auto find_or_insert(const std::string &s) -> size_t {
-        static robin_hood::unordered_map<std::string, size_t> s_to_i{{"", 1}, {" ", 2}};
+        static robin_hood::unordered_map<std::string, size_t> s_to_i{{"", 1U}, {" ", 2U}};
         if (auto tmp = s_to_i.find(s); tmp != s_to_i.end()) return tmp->second;
         s_to_i.emplace(s, SH.size());
         SH.push_back(s);
