b2206e923faa9db805740588f93b54ab62adedd9
Vincent Beffara <vbeffara@gmail.com>
2020-05-08 13:27:18 +0200

    Simpler to just grow Mactab than to shuffle holes around

--- src/Mactab.cpp
+++ src/Mactab.cpp
@@ -7,15 +7,8 @@
 
 auto Mactab::mc_new_macro(Macro *s) -> size_t {
     the_parser.my_stats.one_more_macro();
-    if (next == size()) {
-        push_back({s, 0});
-        return next++;
-    } else {
-        auto w = next;
-        next   = at(w).ref;
-        at(w)  = {s, 0};
-        return w;
-    }
+    push_back({s, 0});
+    return size() - 1;
 }
 
 // Decrements the reference count. If it is zero, we kill the macro.
@@ -28,7 +21,5 @@ void Mactab::delete_macro_ref(size_t i) {
     if (at(i).ref == 0) {
         the_parser.my_stats.one_less_macro();
         delete at(i).ptr;
-        at(i).ref = next;
-        next      = i;
     }
 }
--- src/tralics/Mactab.h
+++ src/tralics/Mactab.h
@@ -12,11 +12,7 @@ struct rc_mac {
 // Consider: \def\mac{\def\mac{a} b}. When mac is expanded, its body is copied
 // when the inner \def is executed, then \mac is destroyed (if nobody else
 // points to it, i.e. if the reference count is zero)
-class Mactab : std::vector<rc_mac> {
-private:
-    size_t next{0}; // pointer to the first free position
-
-public:
+struct Mactab : std::vector<rc_mac> {
     void incr_macro_ref(size_t c) { at(c).ref++; }
     void delete_macro_ref(size_t i);
     auto get_macro(size_t k) -> Macro & { return *at(k).ptr; }
