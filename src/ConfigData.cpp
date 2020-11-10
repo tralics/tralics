2ffd1a4288cd99da8251263bdedb2be5c3bec459
Vincent Beffara <vbeffara@gmail.com>
2020-11-10 22:48:41 +0100

    Get rid of \pers

--- src/ConfigData.cpp
+++ src/ConfigData.cpp
@@ -1,23 +1,7 @@
 #include "tralics/ConfigData.h"
 #include "tralics/Parser.h"
 
-namespace {
-    // Interprets the RC argument of a pers command \todo RA
-    // This returns the short name, said otherwise, the argument.
-    // Notice the space case when argument is empty, or +foo or =foo.
-    auto pers_rc(const std::string &rc) -> std::string {
-        static std::string the_default_rc; // \todo remove
-        if (rc.empty()) return the_default_rc;
-        if (rc[0] == '+') return rc.substr(1);
-        bool spec = (rc.size() >= 2 && rc[0] == '=');
-        auto RC   = spec ? rc.substr(1) : rc;
-        if (spec) the_default_rc = RC;
-        return RC;
-    }
-} // namespace
-
 auto ConfigData::find_one_key(const std::string &name, const std::string &key) const -> std::string {
-    if (name == "ur") return pers_rc(key);
     if (name == "section") return key.empty() ? "default" : key;
     auto it = find(name);
     if (it == end()) {
