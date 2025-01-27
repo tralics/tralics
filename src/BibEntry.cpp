f1470af462f8c029a413a942fdb8f444fb93d22f
Vincent Beffara <vbeffara@gmail.com>
2025-01-27 16:52:39 +0100

    The default value of 'type' for 'patent' should not be "Technical report"

--- src/BibEntry.cpp
+++ src/BibEntry.cpp
@@ -460,7 +460,7 @@ void BibEntry::call_type_all() {
         out_something(fp_type, 3);
     else if (type_int == type_phdthesis)
         out_something(fp_type, 1);
-    else if (type_int == type_techreport || type_int == type_report || type_int == type_patent)
+    else if (type_int == type_techreport || type_int == type_report)
         out_something(fp_type, 2);
     else
         out_something(fp_type);
