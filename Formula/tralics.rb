7366c3ee420c8c018756968d898198e4557836e7
Vincent Beffara <vbeffara@gmail.com>
2026-02-23 11:10:32 +0100

    Update Homebrew formula

--- Formula/tralics.rb
+++ Formula/tralics.rb
@@ -2,6 +2,9 @@ class Tralics < Formula
   desc "LaTeX to XML translator"
   homepage "https://github.com/tralics/tralics"
   license "CECILL-2.1"
+  url "https://github.com/tralics/tralics/archive/refs/tags/v3.0.0.tar.gz"
+  sha256 "eb493eb78dd4e32a64a64b9f0ae3d90202013e11d6a2ba06297e7adbd8f9240f"
+  version "3.0.0"
   head "https://github.com/tralics/tralics.git", branch: "main"
 
   depends_on "cmake" => :build
